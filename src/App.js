8c31b9412a24e78493acecff157e6bcccab82f9e
Angelo Di Massimo <angelo.dimassimo@mrmglobal.com>
2023-12-21 16:09:48 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Workspace from 'polotno/canvas/workspace';
+import { store } from './SetupPolotno';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+            <Workspace store={store} />
     </div>
-  );
-}
+  )
+};
 
 export default App;
